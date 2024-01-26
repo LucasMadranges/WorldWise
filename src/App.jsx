50cc66f11cd55c30e3e9c565b1b6e3055615bc8c
Lucas Madranges <lucas.madranges.pro@icloud.com>
2024-01-26 19:09:05 +0100

    Implementing Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Product from "./pages/Product.jsx";
+import Pricing from "./pages/Pricing.jsx";
+import Homepage from "./pages/Homepage.jsx";
+import PageNotFound from "./pages/PageNotFound.jsx";
+
 export default function App() {
-    const x = 23;
-    
     return (
-        <div>
-            WorldWise
-        </div>
+        <>
+            <div>
+                <h1>hello router!</h1>
+
+                <BrowserRouter>
+                    <Routes>
+                        <Route path="/"
+                               element={<Homepage/>}/>
+                        <Route path="product"
+                               element={<Product/>}/>
+                        <Route path="pricing"
+                               element={<Pricing/>}/>
+                        <Route path="*"
+                               element={<PageNotFound/>}/>
+                    </Routes>
+                </BrowserRouter>
+            </div>
+        </>
     )
 }
