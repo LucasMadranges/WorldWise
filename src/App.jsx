e6ef735290ce5fbff295cdcb013e36674363fafc
Lucas Madranges <lucas.madranges.pro@icloud.com>
2024-01-27 11:49:56 +0100

    Implementing routes for the app page

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ export default function App() {
             <div>
                 <BrowserRouter>
                     <Routes>
-                        <Route path="/"
+                        <Route index
                                element={<Homepage/>}/>
                         <Route path="product"
                                element={<Product/>}/>
@@ -21,7 +21,22 @@ export default function App() {
                         <Route path="login"
                                element={<Login/>}/>
                         <Route path="app"
-                               element={<AppLayout/>}/>
+                               element={<AppLayout/>}>
+                            <Route index
+                                   element={<p>List of cities</p>}/>
+                            <Route path='cities'
+                                   element={
+                                       <p>List of cities</p>
+                                   }/>
+                            <Route path='countries'
+                                   element={
+                                       <p>List of countries</p>
+                                   }/>
+                            <Route path='form'
+                                   element={
+                                       <p>Form</p>
+                                   }/>
+                        </Route>
                         <Route path="*"
                                element={<PageNotFound/>}/>
                     </Routes>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,6 +2,7 @@ import styles from "./Sidebar.module.css";
 import AppNav from "./AppNav.jsx";
 import Logo from "./Logo.jsx";
 import Footer from "./Footer.jsx";
+import {Outlet} from "react-router-dom";
 
 export default function Sidebar() {
     return (
@@ -9,7 +10,8 @@ export default function Sidebar() {
             <Logo/>
             <AppNav/>
 
-            <p>List of cities</p>
+            <Outlet/>
+            
             <Footer/>
         </div>
     )
