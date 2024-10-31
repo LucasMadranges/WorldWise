9c187f1a6009f5a00c94d3bf98d1a3c403af5f9d
Lucas Madranges <lucas.madranges.pro@icloud.com>
2024-10-31 21:36:24 +0100

    :sparkles: feat(Map): Adding Marker from cities list
    Adding map function for displayed the entire list in the leaftlet map

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,29 +1,42 @@
-import styles from './Map.module.css';
+import styles from "./Map.module.css";
 import {useNavigate, useSearchParams} from "react-router-dom";
 import {MapContainer, Marker, Popup, TileLayer} from "react-leaflet";
 import {useState} from "react";
+import {useCities} from "../../contexts/CitiesContext.jsx";
 
 export default function Map() {
     const navigate = useNavigate();
-    const [mapPosition, setMapPosition] = useState([40, 0])
+    const {cities} = useCities();
 
-    const [searchParams, setSearchParams] = useSearchParams()
-    const lat = searchParams.get('lat');
-    const lng = searchParams.get('lng');
+    const [mapPosition, setMapPosition] = useState([40, 0]);
+
+    const [searchParams, setSearchParams] = useSearchParams();
+    const lat = searchParams.get("lat");
+    const lng = searchParams.get("lng");
 
     return (
         <div className={styles.mapContainer}>
-            <MapContainer className={styles.map} center={mapPosition} zoom={13} scrollWheelZoom={true}>
+            <MapContainer className={styles.map}
+                          center={mapPosition}
+                          zoom={13}
+                          scrollWheelZoom={true}>
                 <TileLayer
                     attribution='&copy; <a href="https://www.openstreetmap.fr/hot/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
-                <Marker position={mapPosition}>
-                    <Popup>
-                        A pretty CSS3 popup. <br /> Easily customizable.
-                    </Popup>
-                </Marker>
+                {cities.map((city, index) => <Marker key={index}
+                                                     position={[city.position.lat, city.position.lng]}>
+                        <Popup>
+                            <span>
+                                {city.emoji}
+                            </span>
+                            <span>
+                                {city.cityName}
+                            </span>
+                        </Popup>
+                    </Marker>,
+                )}
             </MapContainer>
         </div>
-    )
+    );
 }
