25bf659a4099f9f7a1f756b045d22581ebabf657
Lucas Madranges <lucas.madranges.pro@icloud.com>
2024-10-31 21:53:06 +0100

    :sparkles: feat(Map): Adding Click event and Position camera
    Adding click event function for display the form when a click is occured on the map
    Adding position camera when a pin is center

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,24 +1,27 @@
 import styles from "./Map.module.css";
 import {useNavigate, useSearchParams} from "react-router-dom";
-import {MapContainer, Marker, Popup, TileLayer} from "react-leaflet";
-import {useState} from "react";
+import {MapContainer, Marker, Popup, TileLayer, useMap, useMapEvents} from "react-leaflet";
+import {useEffect, useState} from "react";
 import {useCities} from "../../contexts/CitiesContext.jsx";
 
 export default function Map() {
-    const navigate = useNavigate();
     const {cities} = useCities();
 
     const [mapPosition, setMapPosition] = useState([40, 0]);
 
     const [searchParams, setSearchParams] = useSearchParams();
-    const lat = searchParams.get("lat");
-    const lng = searchParams.get("lng");
+    const mapLat = searchParams.get("lat");
+    const mapLng = searchParams.get("lng");
+
+    useEffect(() => {
+        if (mapLng && mapLng) setMapPosition([+mapLat, +mapLng]);
+    }, [mapLat, mapLng]);
 
     return (
         <div className={styles.mapContainer}>
             <MapContainer className={styles.map}
                           center={mapPosition}
-                          zoom={13}
+                          zoom={6}
                           scrollWheelZoom={true}>
                 <TileLayer
                     attribution='&copy; <a href="https://www.openstreetmap.fr/hot/copyright">OpenStreetMap</a> contributors'
@@ -36,7 +39,26 @@ export default function Map() {
                         </Popup>
                     </Marker>,
                 )}
+
+                <ChangeCenter position={mapPosition}/>
+                <DetectClick/>
             </MapContainer>
         </div>
     );
+}
+
+function ChangeCenter({position}) {
+    const map = useMap();
+    map.setView(position);
+    return null;
+}
+
+function DetectClick() {
+    const navigate = useNavigate();
+
+    useMapEvents({
+        click: (event) => {
+            navigate(`form?lat=${event.latlng.lat}&lng=${event.latlng.lng}`);
+        },
+    });
 }
